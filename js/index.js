7e5d7073f0b893c48045908707b9863462374f99
fangyuan <136338763@qq.com>
2018-09-09 10:14:48 +0800

    2018-9-8 update

--- js/index.js
+++ js/index.js
@@ -1,28 +1,43 @@
 var width = 0;
 var div = document.getElementsByClassName("sibling-float-item")[0];
+var w = parseInt(getComputedStyle(div.parentNode).width)
 function task(){
-    var w = parseInt(getComputedStyle(div.parentNode).width)
     width -= w;
-    if(width == -3*w){
+    console.log(width);
+    if(width <= -3*w){
         width = 0;
         div.style.transition = "0s"
-        div.style.left = "0px";
+        // div.style.left = "0px";
+        // task();
     }else{
-        div.style.transition = "1s"
-        div.style.left = width+"px";
+        div.style.transition = "2s"
     }
+    div.style.left = width+"px";
 }
-var timer = setInterval(task,2000);
+task();
+var timer = setInterval(task,3000);
 div.onmouseover = function(e){
     var img = e.target;
     if(img.nodeName == "IMG"){
         clearInterval(timer);
+        timer = null;
     }
 }
 div.onmouseout = function(e){
     var img = e.target;
     if(img.nodeName == "IMG"){
-        timer = setInterval(task,2000);
+        timer = setInterval(task,3000);
     }
 }
-console.log(banner.children);
+var leftDiv = document.querySelector("#banner>div.left");
+var rightDiv = document.querySelector("#banner>div.right");
+leftDiv.onclick = function(){
+    var nleft = getComputedStyle(div).left
+    console.log(nleft);
+    div.style.left =  parseInt(nleft) - w + "px";
+}
+rightDiv.onclick = function(){
+    var nleft = getComputedStyle(div).left
+    console.log(nleft);
+    div.style.left = parseInt(nleft) + w +"px";
+}
