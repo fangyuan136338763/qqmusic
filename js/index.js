2b3a5aa15718817c9c53ca2977564ba59a3cc4e0
fangyuan <136338763@qq.com>
2018-09-09 20:33:19 +0800

    javascript

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 var width = 0;
-var div = document.getElementsByClassName("sibling-float-item")[0];
+var aNode = document.querySelector("#banner>ul>li>a[class=active]");
+var id = aNode.getAttribute("data-toggle");
+var div = document.querySelector(`${id}`);
 var w = parseInt(getComputedStyle(div.parentNode).width)
 function task(){
+    var aNode = document.querySelector("#banner>ul>li>a[class=active]");
+    var id = aNode.getAttribute("data-toggle");
+    var div = document.querySelector(`${id}`);
     width -= w;
-    console.log(width);
     if(width <= -3*w){
         width = 0;
         div.style.transition = "0s"
@@ -16,6 +20,7 @@ function task(){
 }
 task();
 var timer = setInterval(task,3000);
+
 div.onmouseover = function(e){
     var img = e.target;
     if(img.nodeName == "IMG"){
@@ -29,15 +34,61 @@ div.onmouseout = function(e){
         timer = setInterval(task,3000);
     }
 }
-var leftDiv = document.querySelector("#banner>div.left");
-var rightDiv = document.querySelector("#banner>div.right");
-leftDiv.onclick = function(){
-    var nleft = getComputedStyle(div).left
-    console.log(nleft);
-    div.style.left =  parseInt(nleft) - w + "px";
+var leftDiv = document.querySelector("#banner div.left");
+var rightDiv = document.querySelector("#banner div.right");
+var iconDiv = document.querySelector("#banner div.icon")
+iconDiv.onmouseover = function(){
+    clearInterval(timer);
+    timer = null;
 }
+iconDiv.onmouseout = function(){
+    timer = setInterval(task,3000);
+}
+leftDiv.onclick = function(){
+    var aNode = document.querySelector("#banner>ul>li>a[class=active]");
+    var id = aNode.getAttribute("data-toggle");
+    console.log(id)
+    var div = document.querySelector(`${id}`);
+    var nleft = getComputedStyle(div).left;
+    nleft  = parseInt(nleft);
+    // console.log(nleft);
+    if(nleft <= -2400){
+        nleft = 1200;
+    }
+    div.style.left = nleft - w + "px";
+}
+
+
 rightDiv.onclick = function(){
-    var nleft = getComputedStyle(div).left
-    console.log(nleft);
-    div.style.left = parseInt(nleft) + w +"px";
+    var aNode = document.querySelector("#banner>ul>li>a[class=active]");
+    var id = aNode.getAttribute("data-toggle");
+    var div = document.querySelector(`${id}`);
+    var nleft = getComputedStyle(div).left;
+    nleft = parseInt(nleft);
+    if(nleft >= 0){
+        nleft = -3600;
+    }
+    div.style.left = nleft + w +"px";
+}
+
+var ul = document.querySelector("#banner ul:nth-child(2)");
+ul.onclick = function(e){
+    e.preventDefault();
+    if(e.target.nodeName == "A"){
+        var aNodes = ul.querySelectorAll("#banner ul:nth-child(2) a");
+        for(var aNode of aNodes){
+            // console.log(aNode);
+            // console.log(aNode.className)
+            aNode.className = "";
+        }
+        e.target.className = "active";
+        var id = e.target.getAttribute("data-toggle");
+        var contents = document.querySelector("#banner div.sibling-float").children;
+        // console.log(contents);
+        for(var content of contents){
+            content.style.display = "none";
+        }
+        var contentId = document.querySelector(`#banner ${id}`);
+        contentId.style.display = "block";
+    }
 }
