01177a6da36a142e3a0569a6a5c4d34cd66a5ef6
fangyuan <136338763@qq.com>
2018-09-24 00:08:13 +0800

    sql update

--- sql/user.sql
+++ sql/user.sql
@@ -8,4 +8,25 @@ CREATE TABLE user(
     upwd VARCHAR(16) NOT NULL,
     phone VARCHAR(16) NOT NULL
 );
-INSERT INTO user VALUES(1,10000,123456,"13666666666")
+INSERT INTO user VALUES(1,10000,123456,"13666666666");
+
+CREATE TABLE music_family(
+    fid INT PRIMARY KEY,
+    family_name CHAR(16),
+
+);
+INSERT INTO music_family VALUES(10,"为你推荐");
+INSERT INTO music_family VALUES(20,"70年代");
+INSERT INTO music_family VALUES(30,"轻音乐"");
+INSERT INTO music_family VALUES(40,"八音盒");
+INSERT INTO music_family VALUES(50,"情歌");
+INSERT INTO music_family VALUES(60,"网络歌曲");
+
+CREATE TABLE commend_list(
+    cid INT PRIMARY KEY AUTO_INCREMENT,
+    details VARCHAR(32),
+    play_counts CHAR(16),
+    pic VARCHAR(32),
+    family_id INT,
+    FOREIGIN KEY(family_id) REFERENCES music_family(fid)
+);
